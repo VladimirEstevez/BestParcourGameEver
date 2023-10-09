1acac48b7b55bc95a5968a8e48ff17d0cfcb3901
Nehad Taha <nehad.m.taha@gmail.com>
2023-10-09 12:04:47 -0400

    Adding animation and modifying the movement.

--- robot_from_the_godot_engine/playere_scene.gd
+++ robot_from_the_godot_engine/playere_scene.gd
@@ -1,33 +1,67 @@
 extends CharacterBody3D
-
-
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const JUMP_VELOCITY = 10
+var twist_input:=0.0;
+var pitch_input:=0.0;
+@export var mouse_sensetivity:=0.001
+@export var pitch_min=0
+@export var pitch_max=-60
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
+@onready var twist_pivot=$twist
+@onready var pitch_pivot=$twist/pitch
 func _ready():
-	$AnimationPlayer.play("Idle")
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
+func _unhandled_input(event:InputEvent)->void:
+		if event is InputEventMouseMotion:
+			if Input.get_mouse_mode()== Input.MOUSE_MODE_CAPTURED:
+				twist_input = -event.relative.x * mouse_sensetivity
+				pitch_input = - event.relative.y * mouse_sensetivity
+				
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	twist_pivot.rotate_y(twist_input)
+	pitch_pivot.rotate_x(pitch_input)
+	pitch_pivot.rotation.x =clamp(
+		pitch_pivot.rotation.x,
+		deg_to_rad(pitch_max),
+		deg_to_rad(pitch_min))
+	pitch_input=0.0
+	twist_input=0.0
+		
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
+		$AnimationPlayer.play("Jump")
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("up") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "up", "down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (twist_pivot.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		print(  atan2(direction.x, direction.z))
+		$Sketchfab_model.rotation.y = atan2(direction.x, direction.z)
+		$CollisionPolygon3D.rotation.y = atan2(direction.x, direction.z)
+		$AnimationPlayer.play("Run")
+		
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		
+		
+		if is_on_floor():
+			$AnimationPlayer.play("Idle")
 
 	move_and_slide()
