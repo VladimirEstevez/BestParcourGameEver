1c4500db88222144224866735dc5e7d2fb74c041
Vladimir <le0679993@crc-lennox.qc.ca>
2023-10-14 19:05:25 -0400

    added music to game and main menu tried to put some order on the folders but something broke

--- Objects/Coin/Scripts/ui.gd
+++ Objects/Coin/Scripts/ui.gd
@@ -4,17 +4,21 @@ var start_time = 0
 var timer_label
 
 func _ready():
-	start_time = Engine.get_frames_drawn()  # Record the start time
+	start_time = Engine.get_frames_drawn() + Engine.get_frames_per_second()  # Record the start time 60 seconds from now
 	timer_label = $Timer/HBoxContainer/Label
 	$AnimationPlayer.play("StartFlash")
+	$"../AudioStreamPlayer3D".play()
+	
 
 func _process(delta):
 	$MarginContainer/HBoxContainer/Label.text = str(Globals.targets_destroyed)
 	var current_time = Engine.get_frames_drawn()
 	var elapsed_frames = current_time - start_time
-	var elapsed_seconds = elapsed_frames * delta
+	var remaining_seconds = 360 - elapsed_frames * delta
 
-	var formatted_time = format_time(elapsed_seconds)
+	var formatted_time = format_time(remaining_seconds)
+	if formatted_time == "00:00.000":
+		get_tree().quit()
 	timer_label.text = "Time: " + formatted_time
 
 func format_time(seconds):
@@ -23,3 +27,4 @@ func format_time(seconds):
 	var milliseconds = int((seconds - int(seconds)) * 1000)
 
 	return str(minutes).pad_zeros(2) + ":" + str(remaining_seconds).pad_zeros(2) + "." + str(milliseconds).pad_zeros(3)
+
--- readme.md.txt
+++ readme.md.txt
@@ -1,1 +1,25 @@
-nothing to see here 
+Main Menu Video:
+Author: Origins Parkour - Mix Tape Vol. 6
+https://www.youtube.com/watch?v=Zb0UwbIAPoY
+
+Main Menu Music:
+	Author: Yurii Kohut 
+	https://pixabay.com/music/drum-n-bass-background-drum-and-bass-117717/
+
+Rusted Wheel:
+	https://polyhaven.com/a/rusted_wheel_rim_01
+	Author: John Hutcheson
+
+Retro Urban Kit:
+	https://www.kenney.nl/assets/retro-urban-kit
+	Author: Kenney
+	
+Robot:
+	https://sketchfab.com/3d-models/robot-from-the-godot-engine-7683fe3d7660474892cccf6b2104841d?sscid=a1k7_fg25y&utm_source=shareasale&utm_medium=affiliate&utm_campaign=742098_1272560
+	Author: Captain Capsule
+
+Platform: 
+	
+Main Music:
+	Author: Nicole Sheldon
+	https://pixabay.com/music/dance-club-anthem-171194/
